d0194aca94e37f9b6228a562b297f5e6c2405943
ravo10 <rassyrelling@gmail.com>
2017-05-14 11:59:06 +0200

    Correct matching translation

---
+++ language/phpmailer.lang-nn.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Norwegian Nynorsk PHPMailer language file: refer to English translation for definitive list
+ * @package PHPMailer
+ */
+
+$PHPMAILER_LANG['authenticate']         = 'SMTP Feil: Kunne ikkje autentisera.';
+$PHPMAILER_LANG['connect_host']         = 'SMTP Feil: Kunne ikkje koble til SMTP tjenar.';
+$PHPMAILER_LANG['data_not_accepted']    = 'SMTP Feil: Datainnhald ikkje akseptert.';
+$PHPMAILER_LANG['empty_message']        = 'Melding kropp tomt';
+$PHPMAILER_LANG['encoding']             = 'Ukjent koding: ';
+$PHPMAILER_LANG['execute']              = 'Kunne ikkje utføre: ';
+$PHPMAILER_LANG['file_access']          = 'Får ikkje tilgang til filen: ';
+$PHPMAILER_LANG['file_open']            = 'Fil Feil: Kunne ikkje åpne filen: ';
+$PHPMAILER_LANG['from_failed']          = 'Følgjande Frå adresse feila: ';
+$PHPMAILER_LANG['instantiate']          = 'Kunne ikkje initialisera post funksjon.';
+$PHPMAILER_LANG['invalid_address']      = 'Ugyldig adresse: ';
+$PHPMAILER_LANG['mailer_not_supported'] = ' sendar er ikkje støtta.';
+$PHPMAILER_LANG['provide_address']      = 'Du må opppgi minst ein mottakaradresse.';
+$PHPMAILER_LANG['recipients_failed']    = 'SMTP Feil: Følgjande mottakaradresse feila: ';
+$PHPMAILER_LANG['signing']              = 'Signering Feil: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP connect() feila.';
+$PHPMAILER_LANG['smtp_error']           = 'SMTP server feil: ';
+$PHPMAILER_LANG['variable_set']         = 'Kan ikkje skrive eller omskrive variabel: ';
+$PHPMAILER_LANG['extension_missing']    = 'Utvidelse manglar: ';
